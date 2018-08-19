6d26d0a886e98c256a46c26c9f83ca038a980eeb
retrorangepi <retrorangepi@outlook.com>
2018-08-19 03:44:24 +0200

    translation fixes

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -201,18 +201,18 @@ bool loadSystemConfigFile(const char** errorString)
 	if(!SystemData::loadConfig())
 	{
 		LOG(LogError) << "Error while parsing systems configuration file!";
-		*errorString = "IT LOOKS LIKE YOUR SYSTEMS CONFIGURATION FILE HAS NOT BEEN SET UP OR IS INVALID. YOU'LL NEED TO DO THIS BY HAND, UNFORTUNATELY.\n\n"
-			"VISIT EMULATIONSTATION.ORG FOR MORE INFORMATION.";
+		*errorString = "PARECE QUE TU ARCHIVO DE CONFIGURACION DE SISTEMAS NO HA SIDO ESTABLECIDO O ES INVALIDO. NECESITARAS HACER ESTO DESAFORTUNADAMENTE.\n\n"
+			"VISITA WWW.RETRORANGEPI.ORG PARA MAS INFORMACION";
 		return false;
 	}
 
 	if(SystemData::sSystemVector.size() == 0)
 	{
 		LOG(LogError) << "No systems found! Does at least one system have a game present? (check that extensions match!)\n(Also, make sure you've updated your es_systems.cfg for XML!)";
-		*errorString = "WE CAN'T FIND ANY SYSTEMS!\n"
-			"CHECK THAT YOUR PATHS ARE CORRECT IN THE SYSTEMS CONFIGURATION FILE, "
-			"AND YOUR GAME DIRECTORY HAS AT LEAST ONE GAME WITH THE CORRECT EXTENSION.\n\n"
-			"VISIT EMULATIONSTATION.ORG FOR MORE INFORMATION.";
+		*errorString = "¡NO SE HA ENCONTRADO NINGUN SISTEMA!\n"
+			"COMPRUEBA QUE TUS RUTAS DE FICHEROS SON CORRECTOS EN LA CONFIGURACION"
+			"Y QUE LA RUTA DE JUEGOS TIENE AL MENOS UN JUEGO CON EXTENSION VALIDA.\n\n"
+			"VISITA WWW.RETRORANGEPI.ORG PARA MAS INFORMACION";
 		return false;
 	}
 
